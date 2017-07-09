b68e8e67180ab75fe056b53540fd0664303edba4
NaiHsuanLin <naihsuan.lin@mail.utoronto.ca>
2017-07-09 19:39:30 -0400

    Add files via upload

---
+++ v_alarm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICalParser;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace ICalParser
+{
+    public class v_alarm
+    {
+        public v_alarm(v_alarm alarm)
+        {
+            this.display = alarm.display;
+            this.trigger = alarm.trigger;
+            this.description = alarm.description;
+        }
+
+        public v_alarm(string Display, string Trigger, string Description)
+        {
+            display = Display;
+            trigger = Trigger;
+            description = Description;
+        }
+
+        public string display { get; set; }
+        public string trigger { get; set; }
+        public string description { get; set; }
+
+    }
+}
---
+++ v_timeZone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICalParser;
+namespace ICalParser {
+    public class v_timeZone
+    {
+        public v_timeZone(string tzid,string tzn, r_rule rRule,int TZOSF, int TZOST,v_daylightSaving DS)
+        {
+            TZID = tzid;
+            TZName = tzn;
+            recursionRule = rRule;
+            TZoffSetFrom = TZOSF;
+            TZoffSetTo = TZOST;
+            daylightSaving = DS;
+        }
+        public string TZID { get; set; }
+        public string TZName { get; set; }
+        public r_rule recursionRule;
+        public int TZoffSetFrom { get; set; }
+        public int TZoffSetTo { get; set; }
+        public v_daylightSaving daylightSaving;
+
+
+    }
+
+}
