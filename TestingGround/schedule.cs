ce161eb120ca9ce5c3c31ef96a09893b61242b97
Eidolon Seven <zpmao77@gmail.com>
2017-07-29 21:10:30 -0230

    Half of the parser
    Just a quick save. Parser is mostly finished with some additional
    adjustments to be made.

--- TestingGround/schedule.cs
+++ TestingGround/schedule.cs
@@ -14,14 +14,172 @@ namespace ICalParser
         //Reads through the document given by the path and generates the members
         public icalSchedule(string icalPath)
         {
+            //intializing the reader
             StreamReader reader = new StreamReader(icalPath);
+
+            //Reads a line from the stream
             string line = reader.ReadLine();
 
+            //Basically weather or not we are reading the write format check
+            if (line != "BEGIN:VCALENDAR") valid = false;
+
+            line = reader.ReadLine();
+
+            //while the file is not exhusted (or bad formating)
             while (line != "END:VCALENDAR")
             {
-                
 
-                line = reader.ReadLine();
+                //temp strings where the number indicates number of digits
+                string tempTwo = null;
+                string tempThree = null;
+                string tempFour = null;
+                string tempFive = null;
+                string tempSix = null;
+                string tempSeven = null;
+                string tempEight = null;
+
+                //Parsing the headers
+                for (int counter = 0; counter < 8 || counter < line.Length-1; counter++)
+                {
+                    tempEight = tempEight + line[counter];
+                    if (counter < 7)
+                    {
+                        tempSeven = tempSeven + line[counter];                   
+                    }
+
+                    if (counter < 6)
+                    {
+                        tempSix = tempSix + line[counter];
+                    }
+
+                    if (counter < 5)
+                    {
+                        tempFive = tempFive + line[counter];
+                    }
+
+                    if (counter < 4)
+                    {
+                        tempFour = tempFour + line[counter];
+                    }
+
+                    if (counter < 3)
+                    {
+                        tempThree = tempThree + line[counter];
+                    }
+                    
+                    if (counter < 2)
+                    {
+                        tempTwo = tempTwo + line[counter];
+                    }
+                }
+
+                //For extentions
+                if (tempTwo == "X-")
+                {
+                    extentionsUsed.Add(line);
+                }
+
+                //for ProductID
+                //remember to fix getting rid of header
+                else if (tempSix == "PRODID")
+                {
+                    productID = line;
+                }
+
+                //For version
+                //remember to fix the header
+                else if (tempSeven == "VERSION")
+                {
+                    version = line;
+                }
+
+                //Nothing else to deal with for now
+                else if (tempEight == "CALSCALE" && line != "CALSCALE:GREGORIAN")
+                {
+                    valid = false;
+                }
+
+                else if (line == "BEGIN:VTIMEZONE")
+                {
+                    while (line != "END:VTIMEZONE")
+                    {
+                        line = reader.ReadLine();
+
+                        string vtimeTemp4 = null;
+                        
+                        for (int timeCounter = 0; timeCounter < 4 || timeCounter < line.Length-1; timeCounter++)
+                        {
+                            vtimeTemp4 = vtimeTemp4 + line[timeCounter];
+
+                        }
+
+                        //REMEMBER TO CLEAN THE HEADER
+                        if (vtimeTemp4 == "TZID")
+                        {
+                            timezone.TZID = line;
+                        }
+
+                        if (line == "BEGIN:DAYLIGHT")
+                        {
+                            while (line != "END:DAYLIGNT")
+                            {
+                                line = reader.ReadLine();
+
+                                string DaylightTemp12 = null;
+                                string DaylightTemp10 = null;
+                                string DaylightTemp8 = null;
+                                string DaylightTemp6 = null;
+                                string DaylightTemp5 = null;
+                                
+                                for (int dayCounter = 0; dayCounter < 12 || dayCounter < line.Length-1; dayCounter++)
+                                {
+                                    DaylightTemp12 = DaylightTemp12 + line[dayCounter];
+                                    if (dayCounter < 10) DaylightTemp10 = DaylightTemp10 + line[dayCounter];
+                                    if (dayCounter < 8) DaylightTemp8 = DaylightTemp8 + line[dayCounter];
+                                    if (dayCounter < 6) DaylightTemp6 = DaylightTemp6 + line[dayCounter];
+                                    if (dayCounter < 5) DaylightTemp5 = DaylightTemp5 + line[dayCounter];
+
+                                }
+
+                                if (DaylightTemp6 == "TZOFFSETFROM")
+                                {
+
+                                }
+
+                                //Catch for exceptions
+                                else
+                                {
+                                    unhandledExeptions.Add(line);
+                                    line = reader.ReadLine();
+                                }
+
+
+
+
+
+                            }
+
+                            
+                        }
+
+                        //Catch for exceptions
+                        else
+                        {
+                            unhandledExeptions.Add(line);
+                            line = reader.ReadLine();
+                        }
+
+
+                    }
+                }
+
+                //Catch for exceptions
+                else
+                {
+                    unhandledExeptions.Add(line);
+                    line = reader.ReadLine();
+                }
+                
             }
         }
 
@@ -35,7 +193,11 @@ namespace ICalParser
         string calScale { get; set; }
         string version { get; set; }
         v_timeZone timezone { get; set; }
-        List<string> exeptionsUsed;
+        List<string> extentionsUsed;
+        List<string> unhandledExeptions;
+
+        //If the schedule is valid
+        bool valid;
 
         private List<vEvent> calEvents;
 
